e9aa16e98f1867084648a4ec3770d9f7b1bac5cf
spatrek03 <spatrek03@gmail.com>
2023-04-10 20:09:02 +0200

    Add project files.

---
+++ ToDo-project/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+List<string> todoList = new List<string>();
+string userInput = "";
+
+while (userInput != "quit")
+{
+    Console.Write("Add meg a teendőd (vagy írja be a 'quit' parancsot a kilépéshez): ");
+    userInput = Console.ReadLine();
+    if (userInput != "quit")
+    {
+        todoList.Add(userInput);
+    }
+}
+
+Console.WriteLine("\nA teendőid:");
+for (int i = 0; i < todoList.Count; i++)
+{
+    Console.WriteLine($"{i + 1}. {todoList[i]}");
+}
+
+while (true)
+{
+    Console.Write("\nAdd meg a törlendő teendő sorszámát (vagy írja be a 'quit' parancsot a kilépéshez): ");
+    userInput = Console.ReadLine();
+    if (userInput == "quit")
+    {
+        break;
+    }
+    int index;
+    if (int.TryParse(userInput, out index) && index > 0 && index <= todoList.Count)
+    {
+        todoList.RemoveAt(index - 1);
+        Console.WriteLine("A teendő sikeresen törölve lett.");
+        Console.WriteLine("\nA maradék teendőid:");
+        for (int i = 0; i < todoList.Count; i++)
+        {
+            Console.WriteLine($"{i + 1}. {todoList[i]}");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Hibás bemenet, kérjük adjon meg egy érvényes sorszámot!");
+    }
+}
+
